bac340f7621f54770677c70031a9437a85901d26
Natsumi Yoshioka <natsumi72336@gmail.com>
2017-01-16 22:34:54 +0900

    最低限ひな形完成

---
+++ js/jsonread.js
@@ -0,0 +1,26 @@
+$(function () {
+	function json_read(level)
+	{
+		var filename = null;
+		switch(level)
+		{
+			case 0:
+			  filename = "../json/easy.json"
+			  break;
+			case 1:
+			  filename = "../json/normal.json"
+        break;
+      case 2:
+        filename = "../json/hard.json"
+        break;
+		}
+
+		$.getJSON(filename,function(rank){
+      for(var i = 0 ; i < 10 ; i++)
+      {
+      	var msg = (i+1) + "位" + "  " + "Name: " + rank[i].name + "  " + "Time: " + rank[i].time;
+      	$(rank_disp).append(msg);
+      }
+		});
+	}
+});
